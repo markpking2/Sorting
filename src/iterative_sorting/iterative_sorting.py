44a598e40108a477c07cf64e3bd04c6f8a4bc558
Mark King <markpkng@gmail.com>
2020-02-14 04:03:24 -0500

    swaps

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,24 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
+        # (hint, can do in 3 loc)
         for j in range(i + 1, len(arr)):
             if(arr[j] < arr[smallest_index]):
                 smallest_index = j
         # TO-DO: swap
-        
+
         if(smallest_index != cur_index):
-            temp = arr[cur_index]
-            arr[cur_index] = arr[smallest_index]
-            arr[smallest_index] = temp
+            arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
-    ## return if empty array or only 1 element
+def bubble_sort(arr):
+    # return if empty array or only 1 element
     if(len(arr) == 0 or len(arr) == 1):
         return arr
 
@@ -29,15 +27,13 @@ def bubble_sort( arr ):
         swapped = False
         for i in range(len(arr) - 1):
             if(arr[i] > arr[i + 1]):
-                temp = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = temp
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 swapped = True
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
